bbdeb9458e8919bd4e63ea236603b042650fd351
02Patrick02 <paku02001@utb.vaxjo.se>
2021-08-25 13:30:26 +0200

    lade till 2 klasser

---
+++ Uppgift2/src/main/java/nu/te4/uppgift2/Hus.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nu.te4.uppgift2;
+
+/**
+ *
+ * @author Elev
+ */
+public class Hus {
+    protected int våningar;
+    protected String adress;
+    protected double längd;
+    protected double bredd;
+    protected float area;
+    
+    public void area(double längd, double bred){
+        
+    }
+}
---
+++ Uppgift2/src/main/java/nu/te4/uppgift2/Skola.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nu.te4.uppgift2;
+
+/**
+ *
+ * @author Elev
+ */
+public class Skola {
+    protected int antalKlassrum;
+    
+    public void area(){
+        
+    }
+    
+    public void antalKlassrum(){
+        
+    }
+    
+}
